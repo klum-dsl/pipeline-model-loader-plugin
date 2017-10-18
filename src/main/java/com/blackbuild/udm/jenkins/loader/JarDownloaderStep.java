294717d1102cc170087fe965879bc09c118832fc
Stephan Pauxberger <stephan@blackbuild.com>
2017-10-18 21:16:03 +0200

    Allow implicit reading of model from classpath

--- src/main/java/com/blackbuild/udm/jenkins/loader/JarDownloaderStep.java
+++ src/main/java/com/blackbuild/udm/jenkins/loader/JarDownloaderStep.java
@@ -1,37 +1,24 @@
 package com.blackbuild.udm.jenkins.loader;
 
+import com.blackbuild.klum.ast.util.FactoryHelper;
 import groovy.lang.GroovyClassLoader;
+import groovy.lang.GroovyObject;
 import groovy.lang.Script;
-import hudson.AbortException;
 import hudson.Extension;
-import hudson.FilePath;
-import hudson.model.Item;
-import hudson.model.Job;
 import hudson.model.Run;
 import hudson.model.TaskListener;
-import hudson.util.FileVisitor;
-import jenkins.model.Jenkins;
-import jenkins.util.VirtualFile;
 import org.jenkinsci.plugins.workflow.cps.CpsFlowExecution;
 import org.jenkinsci.plugins.workflow.flow.FlowExecution;
-import org.jenkinsci.plugins.workflow.libs.ClasspathAdder;
-import org.jenkinsci.plugins.workflow.libs.LibraryAdder;
-import org.jenkinsci.plugins.workflow.libs.LibraryStep;
-import org.jenkinsci.plugins.workflow.steps.*;
+import org.jenkinsci.plugins.workflow.steps.AbstractStepDescriptorImpl;
+import org.jenkinsci.plugins.workflow.steps.AbstractStepImpl;
+import org.jenkinsci.plugins.workflow.steps.AbstractSynchronousStepExecution;
+import org.jenkinsci.plugins.workflow.steps.StepContextParameter;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
-import org.springframework.ui.Model;
 
 import javax.inject.Inject;
-
-import java.io.File;
-import java.io.IOException;
-import java.net.URI;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
-
-import static java.lang.String.format;
 
 /**
  * Created by snpaux on 19.04.2017.
@@ -40,6 +27,7 @@ public class JarDownloaderStep extends AbstractStepImpl {
 
     private final String jobName;
     private String configurationClass;
+    private String modelClass;
 
     @DataBoundConstructor
     public JarDownloaderStep(String jobName) {
@@ -51,12 +39,21 @@ public class JarDownloaderStep extends AbstractStepImpl {
         this.configurationClass = configurationClass;
     }
 
+    @DataBoundSetter
+    public void setModelClass(String modelClass) {
+        this.modelClass = modelClass;
+    }
+
     public String getJobName() {
         return jobName;
     }
 
     public String getConfigurationClass() {
         return configurationClass;
+    }
+
+    public String getModelClass() {
+        return modelClass;
     }
 
     @Extension
@@ -71,7 +68,7 @@ public class JarDownloaderStep extends AbstractStepImpl {
         }
 
         @Override public String getDisplayName() {
-            return "Load a model";
+            return "Load a Klum model";
         }
     }
 
@@ -108,17 +105,35 @@ public class JarDownloaderStep extends AbstractStepImpl {
 
             run.save(); // persist changes to ModelAction
 
-            if (step.getConfigurationClass() == null) {
-                listener.getLogger().println("No configuration class defined");
-                return null;
+            Class<GroovyObject> modelClass = null;
+            Class<Script> configClass = null;
+
+            if (step.getModelClass() != null) {
+                listener.getLogger().println("Loading Model class: " + step.getModelClass());
+                modelClass = (Class<GroovyObject>) loader.loadClass(step.getModelClass());
+            }
+            if (step.getConfigurationClass() != null) {
+                listener.getLogger().println("Executing Model script: " + step.getConfigurationClass());
+                configClass = (Class<Script>) loader.loadClass(step.getConfigurationClass());
             }
 
-            listener.getLogger().println("Executing Model script: " + step.getConfigurationClass());
-            Class<Script> configClass = (Class<Script>) loader.loadClass(step.getConfigurationClass());
+            if (modelClass != null && configClass == null) {
+                listener.getLogger().println("create Model from classpath hint");
+                return FactoryHelper.createFromClasspath(modelClass);
+            }
 
-            return configClass.newInstance().run();
+            if (modelClass == null && configClass != null) {
+                listener.getLogger().println("Create model with explicit script");
+                return modelClass.getMethod("createFrom", Class.class).invoke(null, configClass);
+            }
+
+            if (configClass != null) {
+                listener.getLogger().println("Create model from config script: " + configClass.getName());
+                return configClass.newInstance().run();
+            }
+
+            listener.getLogger().println("No configuration class defined");
+            return null;
         }
-
     }
-
 }
